4f8a73669c1727df257d0064b80aec2439683015
2003congthinh <thinh20033101@gmail.com>
2023-12-26 23:04:40 +0700

    interest updated, second time

--- app/src/main/java/myapk/asm3/Interests.java
+++ app/src/main/java/myapk/asm3/Interests.java
@@ -3,12 +3,95 @@ package myapk.asm3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Interests extends AppCompatActivity {
-
+    private String selectedInterest;
+    private String selectedGender;
+    private String selectedPartner;
+    private Spinner interests;
+    private Spinner gender;
+    private Spinner partner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interests);
+        // Get references to filter spinners
+        // Interest spinner
+        interests = findViewById(R.id.interest);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
+                this,
+                R.array.interest,
+                android.R.layout.simple_spinner_item
+        );
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        interests.setAdapter(adapter1);
+
+        // Gender spinner
+        gender = findViewById(R.id.sex);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                this,
+                R.array.gender,
+                android.R.layout.simple_spinner_item
+        );
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gender.setAdapter(adapter2);
+
+        // Partner spinner
+        partner = findViewById(R.id.interested_partner);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
+                this,
+                R.array.partner,
+                android.R.layout.simple_spinner_item
+        );
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        partner.setAdapter(adapter3);
+
+        // Set up the listeners for the spinners
+        // Interest spinner
+        interests.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedInterest = interests.getSelectedItem().toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle the case when nothing is selected
+            }
+        });
+
+        // Gender spinner
+        gender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedGender = gender.getSelectedItem().toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle the case when nothing is selected
+            }
+        });
+
+        // Partner spinner
+        partner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedPartner = partner.getSelectedItem().toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle the case when nothing is selected
+            }
+        });
+    }
+
+    public void Submit(View view){
+        Log.d("SelectedInterest", selectedInterest);
+        Log.d("SelectedGender", selectedGender);
+        Log.d("SelectedPartner", selectedPartner);
     }
 }
