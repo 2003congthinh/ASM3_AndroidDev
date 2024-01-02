c7b21e1d5053cc5bf3d10ca719725403e9b5b2a4
TripelThreat <102275109+TripelThreat@users.noreply.github.com>
2024-01-02 11:49:52 +0700

    personal bio added

---
+++ app/src/main/java/myapk/asm3/PersonalInfo.java
@@ -0,0 +1,14 @@
+package myapk.asm3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_info);
+    }
+}
