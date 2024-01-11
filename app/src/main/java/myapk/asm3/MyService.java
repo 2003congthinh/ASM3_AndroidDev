210b00a848f48abdbda98c2a3f0cb0f60b44e542
2003congthinh <thinh20033101@gmail.com>
2024-01-11 21:55:26 +0700

    Broadcast on hold

--- app/src/main/java/myapk/asm3/MyService.java
+++ app/src/main/java/myapk/asm3/MyService.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -14,16 +15,19 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.ArrayList;
+
 public class MyService extends Service {
     private static String userEmail = HttpHandler.loginEmail;
     private static String status ="";
     static Location cur_loc;
     protected FusedLocationProviderClient fusedLocationProviderClient;
     protected LocationRequest mLocationRequest;
-    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    private static final long UPDATE_INTERVAL = 20*10000 ;
-    private static final long FASTEST_INTERVAL = 10*10000 ;
+    private ArrayList<Location> locList = new ArrayList<>();;
+    private static final long UPDATE_INTERVAL = 20*1000 ;
+    private static final long FASTEST_INTERVAL = 10*1000 ;
     public MyService() {
+        locList = new ArrayList<>();
     }
 
     @Override
@@ -52,12 +56,24 @@ public class MyService extends Service {
                 cur_loc = locationResult.getLastLocation();
 
                 if (cur_loc != null) {
-                    new PostLocation().execute();
-                    Toast.makeText(MyService.this, "Location: " + cur_loc.getLongitude() + ", " + cur_loc.getLatitude(), Toast.LENGTH_SHORT).show();
+                    if (locList.size() > 2) {
+                        locList.remove(0);
+                    }
+
+                    if (locList.isEmpty() || (locList.get(0).getLatitude() - cur_loc.getLatitude() > 0.1)
+                            && (locList.get(0).getLongitude() - cur_loc.getLongitude() > 0.1)) {
+                        new PostLocation().execute();
+                        Log.d("Location: ", cur_loc.getLongitude() + ", " + cur_loc.getLatitude());
+                        Toast.makeText(MyService.this, "Location: " + cur_loc.getLongitude() + ", " + cur_loc.getLatitude(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    // Add the current location to the list
+                    locList.add(cur_loc);
                 }
             }
         }, null);
     }
+
     private static class PostLocation extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
